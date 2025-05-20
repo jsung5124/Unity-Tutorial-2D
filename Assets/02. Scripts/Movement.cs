bd35ecf5f6028ca1e6785f366cae522444337cb1
jsung5124 <jsung5124@naver.com>
2025-05-20 10:58:09 +0900

    [ADD] Unity Project
    -- Assets
    -- Packages
    -- ProjectSettings

---
+++ Assets/02. Scripts/Movement.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)) // 앞으로 가는 기능
+        {
+            transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.S)) // 뒤로 가는 기능
+        {
+            transform.position += Vector3.back * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.A)) // 왼쪽으로 가는 기능
+        {
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.D)) //오른쪽으로 가는 기능
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+
+    }
+}
