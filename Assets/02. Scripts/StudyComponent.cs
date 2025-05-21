ccbeb962412187f0b0a2ff105720375a76ff7d99
jsung5124 <jsung5124@naver.com>
2025-05-21 16:24:00 +0900

    [STUDY] Array, Component
    2025.05.21 수
    -- 배열 및 리스트
    -- 컴포넌트 설정

---
+++ Assets/02. Scripts/StudyComponent.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class StudyComponent : MonoBehaviour
+{
+    public GameObject obj;
+
+    public string changeName;
+
+
+    void Start()
+    {
+        obj = GameObject.Find("Main Camera");
+
+
+        obj.name = changeName;
+    }
+
+}
