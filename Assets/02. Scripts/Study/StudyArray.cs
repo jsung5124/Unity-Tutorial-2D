df8c1995d6cd627bad96ca864d9cbf6aa7457c54
jsung5124 <jsung5124@naver.com>
2025-05-23 16:28:39 +0900

    [ADD] Turret, Solar
    2025.05.23 금
    -- 터렛 생성
    -- 행성 자전, 공전

---
+++ Assets/02. Scripts/Study/StudyArray.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class StudyArray : MonoBehaviour
+{
+    int number1 = 1;
+    private int number2 = 2;
+    public int number3 = 3;
+
+    [SerializeField]
+    private int number4 = 4;
+
+    [SerializeField] private int number5 = 5;
+
+    void Start()
+    {
+        
+    }
+
+}
---
+++ Assets/02. Scripts/Turret 3D/Movement.cs
@@ -0,0 +1,25 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(h, 0, v);
+        Vector3 normalDir = dir.normalized;
+
+        transform.position += normalDir * moveSpeed * Time.deltaTime;
+
+        transform.LookAt(transform.position + normalDir);
+
+    }
+}
